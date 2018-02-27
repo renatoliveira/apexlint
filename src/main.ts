f1780358625878fba678fae776d4f40b1850f463
Renato <ren811@gmail.com>
2018-02-26 21:03:44 -0300

    Use fs's lstat to check if path is a directory or a file.

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import * as file from "./file"
 if (process.argv.length < 3) {
     console.error("❗️ Should specify which file or folder to run.")
 }
-var mode: String = null;
 
-if (process.argv[2].search('\\.cls') != -1) {
-    console.log("📄 Running on " + process.argv[2] + "...")
-    mode = 'file';
-} else if (process.argv[2].charAt(process.argv[2].length-1) == '/') {
+var mode: String = null
+var pathString: fs.PathLike = process.argv[2]
+
+if (fs.lstatSync(pathString).isDirectory()) {
     console.log("📂 Running on folder " + process.argv[2] + "...")
-    mode = 'folder';
+} else {
+    console.log("📄 Running on file " + process.argv[2] + "...")
 }
