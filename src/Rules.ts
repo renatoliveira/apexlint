6d660d2079c7d79bc78d86058dee1efeabd8f605
Renato <ren811@gmail.com>
2018-03-02 22:06:10 -0300

    Add line error to message.

--- src/Rules.ts
+++ src/Rules.ts
@@ -3,9 +3,9 @@ import { LinterError } from "./LinterError";
 
 export class Rules {
     public static lineLimit (ctx: Context) {
-        ctx.content.forEach(line => {
+        ctx.content.forEach((line, index) => {
             if (line.length > 120) {
-                ctx.addError(new LinterError(undefined, 'Line exceeds the limit of 120 characters.'))
+                ctx.addError(new LinterError(index + 1, 'Line exceeds the limit of 120 characters.'))
             }
         })
     }
