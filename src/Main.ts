bfc43156c5b46abc2d3719cd74743ab4226a68de
Renato Oliveira <ren811@gmail.com>
2018-03-26 14:21:45 -0300

    Added trycatch to evoid long error stacktracing when specifying an invalid path or folder.

--- src/Main.ts
+++ src/Main.ts
@@ -13,18 +13,22 @@ if (process.argv.length < 3) {
     console.log(chalk.yellow('\n\tShould specify a file or folder.') + '\n\n\tUsage:\n\t\t\'apexlint <folder/file>\'\n')
     process.exit()
 }
-if (fs.lstatSync(pathString).isDirectory()) {
-    console.log("Running on folder " + process.argv[2] + "...")
-    fs.readdirSync(pathString).forEach(file => {
-        if (file.match(/\.cls$/i)) {
-            let classFile = new ApexFile(file, pathString + '/' + file)
-            processedFiles.push(classFile)
-        }
-    })
-} else {
-    console.log("Running on file " + process.argv[2] + "...")
-    var apexfile = new ApexFile(process.argv[2], pathString)
-    processedFiles.push(apexfile)
+try {
+    if (fs.lstatSync(pathString).isDirectory()) {
+        console.log("Running on folder " + process.argv[2] + "...")
+        fs.readdirSync(pathString).forEach(file => {
+            if (file.match(/\.cls$/i)) {
+                let classFile = new ApexFile(file, pathString + '/' + file)
+                processedFiles.push(classFile)
+            }
+        })
+    } else {
+        console.log("Running on file " + process.argv[2] + "...")
+        var apexfile = new ApexFile(process.argv[2], pathString)
+        processedFiles.push(apexfile)
+    }
+} catch (error) {
+    console.log(chalk.red(error))
 }
 processedFiles.forEach(file => {
     if (file.getErrorCount() > 0) {
